c2a1c686484891848d4df53e5d848e19416909fb
Jacek <kontakt@jk-design.pl>
2023-11-29 13:10:00 +0100

    RWD - header

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -7,7 +7,7 @@
 
     <meta charset="utf-8">
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="description" content="{{ settings()->get("page_description") }}">
     <meta name="robots" content="{{ settings()->get("page_robots") }}">
     <meta name="author" content="{{ settings()->get("page_author") }}">
